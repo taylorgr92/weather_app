dcee3e7b106a9cf7329fc87b1a83af0253c5045f
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-10-11 18:17:21 -0500

    rearrange container div

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,42 @@
-const searchEl = document.getElementById("search-button");
+var searchEl = document.getElementById("#search");
+var cityInputEl = document.getElementById("#site-search");
+var tempEl = document.getElementById("#temp");
+var humEl = document.getElementById("#hum");
+var windEl = document.getElementById("#wind");
+var todayContainerEl = document.getElementById("#today");
 
-function initSite() {
-  //Assigning api key to variable
+
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var weatherData = cityInputEl.value.trim();
+
+  if (weatherData) {
+    getLocalWeather(weatherData);
+
+    todayContainerEl.textContent = "";
+    cityInputEl.value = "";
+  } else {
+    alert("Please enter a valid city");
+  }
+};
+
+
+//Assigning api key to variable
+var getLocalWeather = function (cityName) {
   var APIKEY = "3c24c9842016c0c1388a842a82cf7f3f";
 
-  function getWeather(cityName) {
-    // Execute a current weather get request from open weather api
-    var url =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      cityName +
-      "&appid=" +
-      APIKEY;
-    fetch(url).then(function (response) {
-      //hide weather card before data is retrieved
-      todayweatherEl.classList.remove("d-none");
-    });
-  }
-}
+  // Execute a current weather get request from open weather api
+  var url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=" +
+    APIKEY;
+  fetch(url).then(function (response) {
+    if (response.ok)
+      response.json().then(function (data) {
+        console.log(data);
+      });
+  });
+};
