1dfc97f952a7f81b4e9a9a8e4b3835730649071e
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-10-11 15:35:38 -0500

    add local weather card

--- src/scripts.js
+++ src/scripts.js
@@ -1,2 +1,19 @@
-var APIKEY="3c24c9842016c0c1388a842a82cf7f3f";
+const searchEl = document.getElementById("search-button");
 
+function initSite() {
+  //Assigning api key to variable
+  var APIKEY = "3c24c9842016c0c1388a842a82cf7f3f";
+
+  function getWeather(cityName) {
+    // Execute a current weather get request from open weather api
+    var url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&appid=" +
+      APIKEY;
+    fetch(url).then(function (response) {
+      //hide weather card before data is retrieved
+      todayweatherEl.classList.remove("d-none");
+    });
+  }
+}
